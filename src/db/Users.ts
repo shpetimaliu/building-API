08b65a91533b513d663d9b0cb5d6c3f58b14df12
Shpetim <shpetimmaliu@gmail.com>
2023-11-13 15:54:20 +0100

    upp

--- src/db/Users.ts
+++ src/db/Users.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   authentication: {
@@ -10,4 +12,26 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-export const userModal = mongoose.model("User", UserSchema);
+const UserModel = model("User", UserSchema);
+
+const getUser = () => UserModel.find();
+const getUserByEmail = (email: String) => UserModel.findOne({ email });
+const getUserBySessionToken = (sessionToken: String) =>
+  UserModel.findOne({ "authentication.sessionToken": sessionToken });
+const getUserById = (id: String) => UserModel.findById(id);
+const createAccount = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+const deleteUserById = (id: String) => UserModel.findOneAndDelete({ _id: id });
+const updateUserById = (id: String, value: Record<string, any>) =>
+  UserModel.findOneAndUpdate(id, value);
+
+export {
+  UserModel,
+  createAccount,
+  deleteUserById,
+  getUser,
+  getUserByEmail,
+  getUserById,
+  getUserBySessionToken,
+  updateUserById,
+};
