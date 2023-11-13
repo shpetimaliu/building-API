854c2ecc421c1b7bf06a76c5d4dc6eadab47e796
Shpetim <shpetimmaliu@gmail.com>
2023-11-13 01:17:59 +0100

    set database

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import http from "http";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(
@@ -11,6 +12,8 @@ app.use(
     credentials: true,
   })
 );
+
+require("dotenv").config();
 
 app.use(compression());
 app.use(cookieParser());
@@ -21,3 +24,14 @@ const server = http.createServer();
 server.listen(3030, () => {
   console.log("Server listen in http://localhost:3030");
 });
+
+mongoose.Promise = Promise;
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.log("Error connecting to the database: ", error);
+  });
