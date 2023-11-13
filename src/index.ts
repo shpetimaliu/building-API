e7d45611adb02b98f38d37182d169a87dda7144d
Shpetim <shpetimmaliu@gmail.com>
2023-11-13 01:02:24 +0100

    Remove nodemon.json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello2");
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer();
+
+server.listen(3030, () => {
+  console.log("Server listen in http://localhost:3030");
+});
